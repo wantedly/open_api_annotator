2aed09b7ad7db23dcea4326d6dedeed4a8844787
Kent Nagata <ngtknt@me.com>
2018-06-02 15:58:37 +0900

    Remove unuseful example

--- spec/open_api_annotator_spec.rb
+++ spec/open_api_annotator_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe OpenApiAnnotator do
   it "has a version number" do
     expect(OpenApiAnnotator::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
