f98c03566073bfa2f17e6e0115de6245448d461c
Kent Nagata <ngtknt@me.com>
2018-06-03 10:38:59 +0900

    Validate config

--- lib/open_api_annotator.rb
+++ lib/open_api_annotator.rb
@@ -30,6 +30,7 @@ module OpenApiAnnotator
 
   def self.configure(&block)
     block.call(config)
+    config.validate!
   end
 
   def self.config
@@ -80,6 +81,59 @@ module OpenApiAnnotator
         ApplicationController
       end
     end
+
+    def validate!
+      validate_info!
+      validate_destination_path!
+      validate_path_regexp!
+      validate_application_controller_class_name!
+      validate_application_serializer_class_name!
+    end
+
+    def validate_info!
+      unless info
+        raise InvalidError, <<~EOL
+          You have to set `OpenApi::Info` to `config.info` like:
+
+          ```
+          OpenApiAnnotator.configure do |config|
+            config.info = OpenApi::Info.new(title: "Book API", version: "1")
+          end
+          ```
+
+          You can see the detail of `OpenApi::Info` in
+          https://www.rubydoc.info/gems/open_api/OpenApi/Info
+        EOL
+      end
+    end
+
+    def validate_destination_path!
+      unless destination_path
+        raise InvalidError, <<~EOL
+          You have to set `config.destination_path` like:
+
+          ```
+          OpenApiAnnotator.configure do |config|
+            config.destination_path = Rails.root.join("api_spec.yml")
+          end
+          ```
+        EOL
+      end
+    end
+
+    def validate_path_regexp!
+      # Do nothing
+    end
+
+    def validate_application_serializer_class_name!
+      # Do nothing
+    end
+
+    def validate_application_controller_class_name!
+      # Do nothing
+    end
+
+    class InvalidError < StandardError; end
   end
 
   class Railtie < ::Rails::Railtie
